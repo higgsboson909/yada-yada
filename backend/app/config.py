7fc593dcd06fa5478eccdb83ca3677e71e53bd17
higgsboson909 <higgsbosson906@gmail.com>
2026-08-28 15:47:54 +0500

    Fix config Settings

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,11 @@
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+# Get the directory where config.py lives, and attach ".env" to it
+env_path = os.path.join(os.path.dirname(__file__), ".env")
+
 _base_config = SettingsConfigDict(
-    env_file="./.env", extra="ignore", env_ignore_empty=True
+    env_file=env_path, extra="ignore", env_ignore_empty=True
 )
 
 
@@ -19,8 +23,8 @@ class DatabaseSettings(BaseSettings):
     POSTGRES_DB: str
     POSTGRES_PORT: int
 
-    REDIS_HOST_OLD: str
-    REDIS_PORT_OLD: int
+    # REDIS_HOST_OLD: str
+    # REDIS_PORT_OLD: int
 
     REDIS_URL: str
     model_config = _base_config
@@ -30,5 +34,5 @@ class DatabaseSettings(BaseSettings):
         return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}?ssl=require"
 
 
-db_settings = DatabaseSettings()
-security_settings = SecuritySettings()
+db_settings = DatabaseSettings()  # pyright: ignore
+security_settings = SecuritySettings()  # pyright: ignore
